d66e8d98b1568ab07fd0cda9434f99171d51436e
Andochunk <androchunk@gmail.com>
2019-01-09 06:40:00 +0530

    Initial commit

---
+++ app/src/main/java/com/androchunk/colorstatelist/MainActivity.java
@@ -0,0 +1,17 @@
+package com.androchunk.colorstatelist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.btn);
+        button.setPressed(true);
+    }
+
+}
